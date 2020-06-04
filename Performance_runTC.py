ae657e55a4b48315619c562d276b2f01c25e7960
maksym.pestriakov <maksym.pestriakov@globallogic.com>
2020-06-04 12:42:34 +0300

    Added decorator

--- Performance_runTC.py
+++ Performance_runTC.py
@@ -2,7 +2,7 @@ import argparse
 import csv
 import glob
 import logging
-import os
+import os.path
 import re
 import shutil
 import subprocess
@@ -10,8 +10,8 @@ import time
 import zipfile
 
 from configs.config import Config
+
 from datetime import datetime
-from os import path
 
 
 def get_parser():
@@ -60,6 +60,17 @@ def get_logger():
     return logger
 
 
+def check_text_decorator(func):
+    """Checks if log's text is not None"""
+    def wrapper(self):
+        if not self.full_text:
+            logger.error("File is empty")
+            return
+        return func(self)
+
+    return wrapper
+
+
 class CLIUploaderParser:
     pattern_time = r"\d{4}[-\d\d]+\s[:?\d{2}]*"
     pattern_version = r"serialNumber for upload: (\d.\d)"
@@ -76,18 +87,18 @@ class CLIUploaderParser:
         self.temp_dir = temp_dir
         self.last_log = self._get_last_log()
         self._make_log_copy()
-        self.log_file = path.join(self.temp_dir, path.basename(self.last_log))
+        self.log_file = os.path.join(self.temp_dir, os.path.basename(self.last_log))
         self.full_text = self._get_log_content()
 
     def _get_last_log(self):
         """Find and return last created log in the logs folder"""
         logger.debug(f"logs_dir = {self.logs_dir}")
-        files_list = list(glob.iglob(path.join(self.logs_dir, "*.log")))
+        files_list = list(glob.iglob(os.path.join(self.logs_dir, "*.log")))
         if not files_list:
             logger.error("Logs not found")
             return
-        last_log = max(files_list, key=path.getctime)
-        logger.debug(f"Selected log file: {path.basename(last_log)}")
+        last_log = max(files_list, key=os.path.getctime)
+        logger.debug(f"Selected log file: {os.path.basename(last_log)}")
 
         return last_log
 
@@ -105,17 +116,14 @@ class CLIUploaderParser:
             logger.info("File reading - DONE")
             return full_text
 
+    @check_text_decorator
     def get_upload_time(self):
         """Looks for the start time and the end time of the upload and returns them"""
-        if not self.full_text:
-            logger.error("File is empty")
-            return 0, 0
-
         first_line = re.search(self.pattern_time, self.full_text[0])
         last_line = re.search(self.pattern_time, self.full_text[-1])
         if not first_line or not last_line:
             logger.error("Matches not found")
-            return 0, 0
+            return
 
         start_time = datetime.strptime(first_line.group(), self.time_format_data)
         end_time = datetime.strptime(last_line.group(), self.time_format_data)
@@ -125,38 +133,33 @@ class CLIUploaderParser:
 
         return start_time, end_time
 
+    @check_text_decorator
     def get_cli_version(self):
-        """Get cli uploader version"""
-        if not self.full_text:
-            logger.error("File is empty")
-            return
-
+        """Gets CLI uploader version"""
         for row in self.full_text:
             search_ver = re.search(self.pattern_version, row)
             if search_ver:
-                logger.info("Reading version - DONE")
-                return search_ver.group(1)
+                version = search_ver.group(1)
+                logger.info(f"CLI version: {version}")
+                return version
         logger.error("Version not found.")
         return "Not found"
 
+    @check_text_decorator
     def get_user(self):
-        """Get uploader user"""
-        if not self.full_text:
-            logger.error("File is empty")
-            return
-
+        """Gets uploader user"""
         for row in self.full_text:
             search_user = re.search(self.pattern_user, row)
             if search_user:
-                logger.info("Reading user - DONE")
+                logger.info(f"User: {search_user.group(2)}")
                 return search_user.group(2)
         logger.error("User not found")
         return "User not found"
 
     def _make_log_copy(self):
-        """Copy found last log to the temp folder"""
+        """Copies found last log to the temp folder"""
         try:
-            if not path.exists(self.temp_dir):
+            if not os.path.exists(self.temp_dir):
                 os.mkdir(self.temp_dir)
                 logger.info(f"Folder {self.temp_dir} has been created")
             shutil.copy(self.last_log, self.temp_dir)
@@ -165,8 +168,12 @@ class CLIUploaderParser:
             logger.exception("System could not create specific folder or copy file")
 
     def data_collect(self, step, data, test_script):
-        """Save data from each log file for analyzing"""
-        start_time, end_time = self.get_upload_time()
+        """Saves data from each log file for analyzing"""
+        upload_time = self.get_upload_time()
+        if not upload_time:
+            start_time, end_time = 0, 0  # sets default parameter
+        else:
+            start_time, end_time = upload_time
         data.append({"№": f"{step + 1}",
                      "Uploader ver": f"{self.get_cli_version()}",
                      "User": f"{self.get_user()}",
@@ -180,7 +187,7 @@ class CLIUploaderParser:
 class PerformanceRun:
     """Class allows run JavaScript test via NATS"""
     NATS_DIR = Config().nats_dir
-    RUN_FILE = path.join(NATS_DIR, "TCRunner", "run.cmd ")
+    RUN_FILE = os.path.join(NATS_DIR, "TCRunner", "run.cmd ")
 
     data = []
 
@@ -190,25 +197,25 @@ class PerformanceRun:
 
     def get_script_to_run(self):
         """Chose a correct manifest in the test case"""
-        script_location = path.join(
+        script_location = os.path.join(
             self.NATS_DIR, "_Tests_NGP", self.TEST_NAME_JS
         )
 
-        script_js = path.join(
+        script_js = os.path.join(
             script_location, "manifest.json"
         )
-        script_js_1880 = path.join(
+        script_js_1880 = os.path.join(
             script_location, "manifest_770_1880.json"
         )
-        script_js_1884 = path.join(
+        script_js_1884 = os.path.join(
             script_location, "manifest_780_1884.json"
         )
 
-        if path.isfile(script_js_1880):
+        if os.path.isfile(script_js_1880):
             return script_js_1880
-        elif path.isfile(script_js):
+        elif os.path.isfile(script_js):
             return script_js
-        elif path.isfile(script_js_1884):
+        elif os.path.isfile(script_js_1884):
             return script_js_1884
         else:
             logger.exception("File not found!")
@@ -216,7 +223,7 @@ class PerformanceRun:
     def save_to_csv(self, file_name="Results.csv"):
         """Save data to .csv file"""
         try:
-            with open(path.join(self.temp_dir, file_name),  mode="w", newline="") as file:
+            with open(os.path.join(self.temp_dir, file_name),  mode="w", newline="") as file:
                 fields = self.data[0].keys()
                 writer = csv.DictWriter(file, fieldnames=fields)
 
@@ -230,10 +237,10 @@ class PerformanceRun:
         """Archiving log and scv files and delete after that"""
         try:
             name = f"Performance_{self.TEST_NAME_JS}_{uploader_version}.zip"
-            with zipfile.ZipFile(path.join(self.temp_dir, name), "w") as zip_file:
-                for file in glob.glob(path.join(self.temp_dir, "*.*")):
+            with zipfile.ZipFile(os.path.join(self.temp_dir, name), "w") as zip_file:
+                for file in glob.glob(os.path.join(self.temp_dir, "*.*")):
                     if file.endswith(".log") or file.endswith(".csv"):
-                        file_name = str(path.basename(file))
+                        file_name = str(os.path.basename(file))
                         zip_file.write(file, file_name)
                         logger.info(f"File {file_name} has been archived")
                         os.remove(file)
@@ -262,7 +269,7 @@ class PerformanceRun:
 
                 log_parser.data_collect(step, self.data, self.TEST_NAME_JS)
 
-                logger.info("Time = ", self.data[-1].get("Duration"))
+                logger.info("Duration = %s", self.data[-1].get("Duration"))
 
                 if step != (run_times - 1):
                     logger.info("Waiting 10 sec...")
@@ -278,8 +285,8 @@ class PerformanceRun:
 
         except OSError as err:
             logger.error(f"OS error: {0}".format(err), exc_info=True)
-        except Exception:
-            logger.error("other error", exc_info=True)
+        except Exception as e:
+            logger.error("other error %s", e)
 
 
 if __name__ == "__main__":
